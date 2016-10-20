e59aaf846bbe60465c2412ff8778c7f79c7682f6
tisha1 <T@Guests-MacBook-Pro.local>
2016-10-20 11:33:43 -0400

    added parallax code
    fr. Sara Vieira

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 
+	function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('#hero').css('top', -(scrolled * 0.1) + 'px');
+	}
 
+	$(window).scroll(function(e){
+	    parallax();
+	});
 
 
 	
