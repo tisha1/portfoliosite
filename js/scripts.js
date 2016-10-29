5f399ddba9df63a6ccefed354e03f73047155368
tisha1 <T@Guests-MacBook-Pro.local>
2016-10-29 14:27:43 -0400

    smooth scroll and initial animation

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,18 @@ $(document).ready(function() {
 	});
 
 
-	
+	$('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+  });
 
 
 
