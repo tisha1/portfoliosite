a0788785189d247f2649ead3e039f5bab239205f
tisha1 <T@Guests-MacBook-Pro.local>
2016-10-31 20:56:50 -0400

    green letter version

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
-	function parallax(){
-    var scrolled = $(window).scrollTop();
-    $('#hero').css('margin-top', '100px');
-    $('#hero').css('top', -(scrolled * 0.3) + 'px');
-	}
+	// function parallax(){
+ //    var scrolled = $(window).scrollTop();
+ //    $('#hero').css('margin-top', '100px');
+ //    $('#hero').css('top', -(scrolled * 0.3) + 'px');
+	// }
 
 
-	$(window).scroll(function(e){
-	    parallax();
-	});
+	// $(window).scroll(function(e){
+	//     parallax();
+	// });
 
 
 	$('.smoothScroll').click(function() {
